1cd6056981c6b1d5c3b295ebc4b0434a7d8703ad
AntonioZampieri <Antonio.zampieri.1996@gmail.com>
2023-09-13 01:47:11 +0200

    add getMessages method

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,5 +1,3 @@
-//using System.Web.Http;
-
 using Publisher.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +5,19 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+List<Channel> channels = new List<Channel>();
+
 app.MapPost("api/channels/{channelName}/subscribe", async (string channelName, Subscriber sub) => 
 {
     throw new NotImplementedException();
 });
 
 app.MapPost("api/channels/{channelName}/publish", async (string channelName, Message msg) =>
+{
+    throw new NotImplementedException();
+});
+
+app.MapGet("api/subscribers/{subscriberName}/messages", async (string subscriberName) =>
 {
     throw new NotImplementedException();
 });
