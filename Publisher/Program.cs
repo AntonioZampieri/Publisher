6b087c0ffb6729de6013d0d2917df1a2e257c128
AntonioZampieri <Antonio.zampieri.1996@gmail.com>
2023-09-13 02:49:45 +0200

    add subscribe method

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -9,7 +9,21 @@ List<Channel> channels = new List<Channel>();
 
 app.MapPost("api/channels/{channelName}/subscribe", async (string channelName, Subscriber sub) => 
 {
-    throw new NotImplementedException();
+    Channel channel = channels.FirstOrDefault(c => c.channelName == channelName);
+
+    if (channel == null)
+    {
+        channel = new Channel 
+        { 
+            channelName = channelName,
+            messages = new List<Message>(),
+            subscribers = new List<Subscriber>()
+        };
+        
+        channels.Add(channel);
+    }
+
+    channel.subscribers.Add(sub);
 });
 
 app.MapPost("api/channels/{channelName}/publish", async (string channelName, Message msg) =>
@@ -19,7 +33,8 @@ app.MapPost("api/channels/{channelName}/publish", async (string channelName, Mes
 
 app.MapGet("api/subscribers/{subscriberName}/messages", async (string subscriberName) =>
 {
-    throw new NotImplementedException();
+    return new List<string>();
+    //throw new NotImplementedException();
 });
 
 app.Run();
